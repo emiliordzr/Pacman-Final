74b2496399c270eeb95f49c8622dc3669f7cc43c
Omar-Atriano <omar.atrianova@udlap.mx>
2023-02-07 09:54:09 -0600

    .

--- Form1.cs
+++ Form1.cs
@@ -208,7 +208,7 @@ namespace pac_man
 
         public void level1()
         {
-            if (level > 3)
+            if (level > 4)
                 gameend = true;
             DrawMap(level);
             pacman.Image = MyResource.fijo;
@@ -254,7 +254,7 @@ namespace pac_man
 
 
                         }
-                        else if (mapaBase.map0[y, x] == 3 || mapaBase.map0[y, x] == 0)
+                        else if (mapaBase.map0[y, x] == 3 /*|| mapaBase.map0[y, x] == 0*/)
                         {
                             Dot dot = new Dot(new PointF(x * 15 + 7.5f, y * 15 + 7.5f));
                             dot.hb.Add(new Line(new PointF(x * 15, y * 15), new PointF(x * 15 + 15, y * 15)));
@@ -309,7 +309,7 @@ namespace pac_man
 
 
                         }
-                        else if (mapaL2.map0[y, x] == 3 || mapaL2.map0[y, x] == 0)
+                        else if (mapaL2.map0[y, x] == 3 /*|| mapaL2.map0[y, x] == 0*/)
                         {
                             Dot dot = new Dot(new PointF(x * 15 + 7.5f, y * 15 + 7.5f));
                             dot.hb.Add(new Line(new PointF(x * 15, y * 15), new PointF(x * 15 + 15, y * 15)));
@@ -363,7 +363,7 @@ namespace pac_man
 
 
                         }
-                        else if (mapaL3.map0[y, x] == 3 || mapaL3.map0[y, x] == 0)
+                        else if (mapaL3.map0[y, x] == 3 /*|| mapaL3.map0[y, x] == 0*/)
                         {
                             Dot dot = new Dot(new PointF(x * 15 + 7.5f, y * 15 + 7.5f));
                             dot.hb.Add(new Line(new PointF(x * 15, y * 15), new PointF(x * 15 + 15, y * 15)));
