6ecc0b13dd4d9b50c02d3bfa33bf519483d7f57b
Emilio Rodriguez <emilio.rodriguezrz@udlap.mx>
2023-02-07 00:29:45 -0600

    cambios

---
+++ Ghosts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pac_man
+{
+    public class Ghosts
+    {
+        public List<Ghost> ghosts;
+
+        public Ghosts() 
+        {
+            ghosts = new List<Ghost>();
+        }
+    }
+}
